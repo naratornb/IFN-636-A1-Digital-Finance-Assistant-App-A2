9fbfcac51719f2c40bb66ffa4ac81e5b05ac4822
IAmInevitableLeo <likhungleung@gmail.com>
2025-08-11 00:39:13 +1000

    Report Feature(Epic) added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,10 @@ import Navbar from './components/Navbar';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Profile from './pages/Profile';
-import Tasks from './pages/Tasks';
+import Reports from './pages/Reports';
+import Budget from './pages/Budget';
+import Goal from './pages/Goal';
+import Expense from './pages/Expense';
 
 function App() {
   return (
@@ -13,7 +16,10 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/profile" element={<Profile />} />
-        <Route path="/tasks" element={<Tasks />} />
+        <Route path="/reports" element={<Reports />} />
+        <Route path="/budget" element={<Budget />} />
+        <Route path="/goal" element={<Goal />} />
+        <Route path="/expense" element={<Expense />} />
       </Routes>
     </Router>
   );
