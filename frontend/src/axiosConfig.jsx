7789a28fab70c9d552f17f634425266e4954a4b7
IAmInevitableLeo <likhungleung@gmail.com>
2025-08-12 20:45:38 +1000

    updated sever

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   //baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://54.206.112.70:5001', // live
+  baseURL: 'http://3.27.222.15:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
