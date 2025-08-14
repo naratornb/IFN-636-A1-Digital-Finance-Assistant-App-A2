a35c4984b835fdd2bbaa68124c5744ae4cee0288
IAmInevitableLeo <likhungleung@gmail.com>
2025-08-14 21:36:09 +1000

    the seven times chagne the IP

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   //baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://13.239.3.195', // live
+  baseURL: 'http://3.107.193.123', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
