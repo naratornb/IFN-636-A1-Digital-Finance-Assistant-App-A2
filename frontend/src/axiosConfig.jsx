3b99fefcbdd54a2b8dcaa3dcef9543e3690ee11d
IAmInevitableLeo <likhungleung@gmail.com>
2025-08-14 22:20:35 +1000

    add ip again and againgaingaignainingisadnigns

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   //baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://3.107.193.123', // live
+  baseURL: 'http://3.107.193.123:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
