852ce2bd9a98072aebfe0f318769825d22f5eeff
naratorn.b <naratorn.b@kasikornline.com>
2025-09-24 15:56:26 +1000

    fix: edit axiosconfig and ci.yml for cicd

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
+const ip = process.env.ENV_NAME === 'production' ? 'http://54.66.213.198:5001': 'http://localhost:5001';
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5001', // local
-  // baseURL: 'http://13.210.90.148:5001', // live
+  // baseURL: 'http://localhost:5001', // local
+  baseURL: ip,
   headers: { 'Content-Type': 'application/json' },
 });
 
